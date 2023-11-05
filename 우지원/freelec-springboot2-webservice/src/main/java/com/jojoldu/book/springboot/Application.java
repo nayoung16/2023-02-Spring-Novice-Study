1612318665f2681533ac380198f55070c1b1d631
jiwonniy <jiwoni0707@gmail.com>
2023-11-05 21:42:58 +0900

    Test:[1주차] 테스트 코드 작성

--- 우지원/freelec-springboot2-webservice/src/main/java/com/jojoldu/book/springboot/Application.java
+++ 우지원/freelec-springboot2-webservice/src/main/java/com/jojoldu/book/springboot/Application.java
@@ -1,5 +1,4 @@
 package com.jojoldu.book.springboot;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
--- 우지원/freelec-springboot2-webservice/src/test/java/com/jojoldu/book/springboot/web/HelloControllerTest.java
+++ 우지원/freelec-springboot2-webservice/src/test/java/com/jojoldu/book/springboot/web/HelloControllerTest.java
@@ -13,6 +13,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(controllers = HelloController.class)
 public class HelloControllerTest {
