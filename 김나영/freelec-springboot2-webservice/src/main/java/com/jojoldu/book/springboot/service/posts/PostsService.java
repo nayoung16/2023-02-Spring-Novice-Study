d51dce4fc26ebb3d4d26078ed9b353bad7f07c67
김나영 <nykim1016@sookmyung.ac.kr>
2023-11-19 22:34:17 +0900

    Feat : 게시굴 삭제  기능

--- 김나영/freelec-springboot2-webservice/src/main/java/com/jojoldu/book/springboot/service/posts/PostsService.java
+++ 김나영/freelec-springboot2-webservice/src/main/java/com/jojoldu/book/springboot/service/posts/PostsService.java
@@ -42,4 +42,9 @@ public class PostsService {
                 .collect(Collectors.toList());
     }
 
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("헤당 게시글이 없습니다. id=" + id));
+        postsRepository.delete(posts);
+    }
 }
