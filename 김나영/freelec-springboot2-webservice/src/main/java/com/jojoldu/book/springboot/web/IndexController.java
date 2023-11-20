2ee2168f3f0096a2277160aba5770b3fa4062d72
김나영 <nykim1016@sookmyung.ac.kr>
2023-11-20 21:37:02 +0900

    Fix : 404 Error

--- 김나영/freelec-springboot2-webservice/src/main/java/com/jojoldu/book/springboot/web/IndexController.java
+++ 김나영/freelec-springboot2-webservice/src/main/java/com/jojoldu/book/springboot/web/IndexController.java
@@ -18,6 +18,11 @@ public class IndexController {
         return "index";
     }
 
+    @GetMapping("/posts/save")
+    public String postsSave() {
+        return "posts-save";
+    }
+
     @GetMapping("/posts/update/{id}")
     public String postsUpdate(@PathVariable Long id, Model model) {
         PostsResponseDto dto = postsService.findById(id);
@@ -25,5 +30,4 @@ public class IndexController {
 
         return "posts-update";
     }
-
 }
